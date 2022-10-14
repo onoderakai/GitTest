cd7901f4d683a6d104450e7d158a6049527d1aa0
onodera <115197398+onoderakai@users.noreply.github.com>
2022-10-14 09:42:32 +0900

    なんかミスったのでもう一回branch作成

--- GitTest.cpp
+++ GitTest.cpp
@@ -7,6 +7,8 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!!\n";
     std::cout << "Hello New Branch\n";
+    std::cout << "Hello New Branch!\n";
+    std::cout << "Hello New Branch2!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
