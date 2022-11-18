f3edd479a43423ab691dadb632acdfa388431219
onoderakai <k022g1011@g.neec.ac.jp>
2022-11-18 09:34:29 +0900

    ローカルでコンフリクト変更

--- GitTest.cpp
+++ GitTest.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello RemoteTest VisualStudio Git\n";
+    std::cout << "Hello ConflictChanged Git\n";
     std::cout << "VisualStudio Git\n";
     std::cout << "New Branch Changed!\n";
     std::cout << "New Branch\n";
